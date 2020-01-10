23100809729401a3c32a0d1ab977cd79b4a81236
deemo <deemo.chen@yeahmobi.com>
2020-01-10 12:38:33 +0800

    ..

--- sample/app/src/main/java/co/hyperverge/hypersnapsample/HyperSnapSampleApp.java
+++ sample/app/src/main/java/co/hyperverge/hypersnapsample/HyperSnapSampleApp.java
@@ -12,7 +12,7 @@ public class HyperSnapSampleApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        HyperSnapSDK.init(this, "", "", HyperSnapParams.Region.India);
+        HyperSnapSDK.init(this, "12b169", "e431183393f7d9f44581", HyperSnapParams.Region.India);
     }
 
  }
